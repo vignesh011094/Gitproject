889a42e2811c6e5d6d6903897021c5aa9679cc7b
vignesh011094 <vigmeshwarananbu@gmail.com>
2023-05-04 00:13:46 +0530

    new code

---
+++ src/org/tcs/test/ClassDetails.java
@@ -0,0 +1,22 @@
+package org.tcs.test;
+
+public class ClassDetails {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(" Class is Git ");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+
+}
